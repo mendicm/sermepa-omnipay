b7880d57624b1361773b4a4f8a78a792d8f799c6
mendicm <mendicm@gmail.com>
2016-12-19 12:37:59 +0100

    Update PurchaseResponse.php

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -4,6 +4,9 @@ namespace Omnipay\Sermepa\Message;
 
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RedirectResponseInterface;
+use Omnipay\Common\Exception\RuntimeException;
+use Symfony\Component\HttpFoundation\RedirectResponse as HttpRedirectResponse;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 /**
  * Sermepa (Redsys) Purchase Response
@@ -48,5 +51,36 @@ class PurchaseResponse extends AbstractResponse implements RedirectResponseInter
     public function getRedirectUrl()
     {
         return $this->getRequest()->getEndpoint();
+    }
+
+    /**
+     * Overrides original getRedirectResponse() to customize redirect form
+     * @return HttpRedirectResponse | HttpResponse
+     * @throws RuntimeException
+     */
+    public function getRedirectResponse()
+    {
+        if (!$this instanceof RedirectResponseInterface || !$this->isRedirect()) {
+            throw new RuntimeException('This response does not support redirection.');
+        }
+
+        if ('GET' === $this->getRedirectMethod()) {
+            return HttpRedirectResponse::create($this->getRedirectUrl());
+        } elseif ('POST' === $this->getRedirectMethod()) {
+            $hiddenFields = '';
+            foreach ($this->getRedirectData() as $key => $value) {
+                $hiddenFields .= sprintf(
+                    '<input type="hidden" name="%1$s" value="%2$s" />',
+                    htmlentities($key, ENT_QUOTES, 'UTF-8', false),
+                    htmlentities($value, ENT_QUOTES, 'UTF-8', false)
+                ) . "\n";
+            }
+
+            $view = \View::make('omnipay.redsys', ['url' => $this->getRedirectUrl(), 'fields' => $hiddenFields]);
+
+            return HttpResponse::create($view->render());
+        }
+
+        throw new RuntimeException('Invalid redirect method "'.$this->getRedirectMethod().'".');
     }
 }
