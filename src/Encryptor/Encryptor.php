d887389121cbb76031cd2084d0de0ed7e1dbae78
mendicm <mendicm@gmail.com>
2018-06-14 10:42:46 +0200

    Switch mcrypt to openssl. Now php >= 7.1 is supported.

--- src/Encryptor/Encryptor.php
+++ src/Encryptor/Encryptor.php
@@ -7,11 +7,12 @@ class Encryptor
     public static function encrypt_3DES($message, $key)
     {
         $l = ceil(strlen($message) / 8) * 8;
-        
-        return substr(
-            openssl_encrypt($message . str_repeat("\0", $l - strlen($message)), 'des-ede3-cbc', $key, OPENSSL_RAW_DATA, "\0\0\0\0\0\0\0\0"),
-            0,
-            $l
-        );
+        return substr(openssl_encrypt(
+            $message . str_repeat("\0", $l - strlen($message)),
+            'des-ede3-cbc',
+            $key,
+            OPENSSL_RAW_DATA,
+            "\0\0\0\0\0\0\0\0"
+        ), 0, $l);
     }
 }
